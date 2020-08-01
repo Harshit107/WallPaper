c0b2b760eb21a62ccd71ab6bd77afefd2b97ea6f
Harshit Keshari <harshit107.in@gmail.com>
2020-08-02 00:49:26 +0530

    Second Commit

--- app/src/main/java/com/harshit/wallpaper/MainActivity.java
+++ app/src/main/java/com/harshit/wallpaper/MainActivity.java
@@ -6,37 +6,77 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.harshit.wallpaper.Fragment.ImageList;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.harshit.wallpaper.Adapter.LoadImageRecyclearAdapter;
 import com.infideap.drawerbehavior.Advance3DDrawerLayout;
 import com.mancj.materialsearchbar.MaterialSearchBar;
+import com.sdsmdg.tastytoast.TastyToast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import pl.droidsonroids.gif.GifImageView;
 
-    FrameLayout container;
+public class MainActivity extends AppCompatActivity  {
 
+    EditText searchValue;
+    ImageView search_bt;
     DrawerLayout drawer;
     private ActionBarDrawerToggle toggle;
+
+    private RecyclerView mRecyclearView;
+    private LoadImageRecyclearAdapter loadImageRecyclearAdapter;
+    private ArrayList<ItemList> itemLists;
+    private RequestQueue requestQueue=null;
+
+    RelativeLayout loading;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        container = findViewById(R.id.container);
 
+        searchValue = findViewById(R.id.searchValue);
+        search_bt = findViewById(R.id.search_bt);
+        loading = findViewById(R.id.progress);
+        loading.setVisibility(View.GONE);
+
+        mRecyclearView = findViewById(R.id.imageRecycle);
+        mRecyclearView.setHasFixedSize(true);
+        mRecyclearView.setLayoutManager(new LinearLayoutManager(this));
+        itemLists = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(this);
+
+        //---------------   ToolBar and Navigation drawer   ---------------
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         drawer = (Advance3DDrawerLayout) findViewById(R.id.drawer_layout);
         toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -44,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
         toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         ((Advance3DDrawerLayout) drawer).setViewRotation(GravityCompat.START, 15);
         //((Advance3DDrawerLayout) drawer).setRadius(GravityCompat.START, 25);
         ((Advance3DDrawerLayout) drawer).setViewScale(GravityCompat.START, 0.9f); //set height scale for main view (0f to 1f)
@@ -54,7 +93,21 @@ public class MainActivity extends AppCompatActivity {
         ((Advance3DDrawerLayout) drawer).setContrastThreshold(3); //set maximum of contrast ratio between white text and background color.
         ((Advance3DDrawerLayout) drawer).setRadius(GravityCompat.START, 25); //set end container's corner radius (dimension)
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.contatiner, new ImageList()).commit();
+        //---------------------  End --------------------
+        parseJSON("");
+
+        search_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String s = searchValue.getText().toString().trim();
+                Log.d("TAG",s);
+                parseJSON(s);
+            }
+        });
+
+
+      //  getSupportFragmentManager().beginTransaction().replace(R.id.contatiner, new ImageList()).commit();
 
     }
 
@@ -105,4 +158,59 @@ public class MainActivity extends AppCompatActivity {
         toggle.onConfigurationChanged(newConfig);
     }
 
-}
+    private void parseJSON(String s) {
+
+        loading.setVisibility(View.VISIBLE);
+        itemLists.clear();
+        Log.d("TAG",s);
+        String url = "https://pixabay.com/api/?key=17722502-a97d71ebd63cefda99f7e1b20&q="+s+"&image_type=photo&pretty=true&per_page=50";
+
+        JsonObjectRequest request ;
+            request = new JsonObjectRequest(Request.Method.GET
+                    , url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+
+                    try {
+
+                        JSONArray jsonArray = response.getJSONArray("hits");
+                        Log.d("TAG",jsonArray.toString());
+                        for (int i =0; i<jsonArray.length();i++ ){
+
+                            JSONObject hits = jsonArray.getJSONObject(i);
+
+                            int likes = hits.getInt("likes");
+                            int doenload = hits.getInt("downloads");
+                            int favorites = hits.getInt("favorites");
+                            String previewURL = hits.getString("webformatURL");
+                            String downloadImage = hits.getString("webformatURL");
+                            int views = hits.getInt("views");
+                            int id = hits.getInt("id");
+
+                            itemLists.add( new ItemList(favorites,likes,doenload,views,previewURL,downloadImage,id));
+
+                            loadImageRecyclearAdapter = new LoadImageRecyclearAdapter(getApplicationContext(),itemLists);
+                            mRecyclearView.setAdapter(loadImageRecyclearAdapter);
+                        }
+                        loading.setVisibility(View.GONE);
+                    } catch (JSONException e) {
+                        loading.setVisibility(View.GONE);
+                        TastyToast.makeText(getApplicationContext(),e.getMessage(), Toast.LENGTH_LONG,TastyToast.ERROR);
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    loading.setVisibility(View.GONE);
+                    TastyToast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG,TastyToast.ERROR);
+                }
+            });
+
+            requestQueue.add(request);
+        }
+    }
+
+
+
+
