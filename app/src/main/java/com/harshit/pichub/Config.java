83ebc966b5a5b425ad4d14012c89ca6095993417
Harshit keshari <65784019+Harshit107@users.noreply.github.com>
2020-08-09 23:29:36 +0530

    Update Config.java

--- app/src/main/java/com/harshit/pichub/Config.java
+++ app/src/main/java/com/harshit/pichub/Config.java
@@ -1,7 +1,7 @@
 package com.harshit.pichub;
 
 public class Config {
-    public static final String FURLL = "https://pixabay.com/api/?key=17722502-a97d71ebd63cefda99f7e1b20&" ;
+    public static final String FURLL = "https://pixabay.com/api/?key=Your Api Key&" ;
     public static final String LLURL = "&image_type=photo&pretty=true&safesearch=true";;
 
 }
